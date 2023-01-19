53345c4bceedc3eb1ec46bd765e26c5a16f69d69
Wren Wightman <wren.wightman@duke.edu>
2023-01-19 16:50:31 -0500

    Initial commit

---
+++ Beamformer/Beamformer.py
@@ -0,0 +1,50 @@
+
+import numpy as np
+class Beamformer:
+    
+    def __init__(self):
+        """Initialize the beamformer with needed parameters
+        
+        Parameters:
+        ----
+        Nt: number of time samples per RF line (int)
+        dt: sampling period of channel data [s]
+        tstart: start time of rf line [s]
+        usf: upsample factor (int) newsamples/old
+        nele: number of elements in transducer
+        dele: element pitch [m]
+        Nlat: number of lateral, reconstructed pixels (int)
+        dlat: lateral spacing of pixels [m]
+        Nax: number of axial pixels (int)
+        dax: spacing between axial pixels [m]
+        ulc: spatial location of upper left corner [m, m]
+        apod: apodization function (default None)
+        fnum: fnumber used in recon (default None)
+        """
+        self.Nt = None
+        self.dt = None
+        self.tstart = None
+        self.usf = None
+        self.nele = None
+        self.dele = None
+        self.Nlat = None
+        self.dlat = None
+        self.Nax = None
+        self.dax = None
+        self.ulc = None
+        self.apod = None
+        self.fnum = None
+
+    def gentabs(self):
+        # Verify this beamformer has been instantiated properly
+        print(getattr(self,'Nt'))
+
+        # Calculate the lateral position of elements, and the axial/lateral coordinates of recon pixels
+        self.posele = np.linspace(-self.dele*(self.nele-1)/2, self.dele*(self.nele-1)/2, self.nele)
+        self.posax = np.linsapce(self.ulc[0], self.ulc[0] + self.Nax*self.dax, self.Nax)
+        self.poslat = np.linsapce(self.ulc[1], self.ulc[1] + self.Nlat*self.dlat, self.Nlat)
+        self.t = self.tstart + self.dt * np.arange(self.Nt*self.usf + 1) / self.usf # one additional data point to set to zero
+
+oop = Beamformer()
+
+oop.gentabs()
