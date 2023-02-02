e43fb0d5397c7ad6eaf32ac4d480124e99f16511
Wren Wightman <wren.wightman@duke.edu>
2023-02-01 21:51:50 -0500

    Added testing for pwtxengine

--- tests/test_trigengines.py
+++ tests/test_trigengines.py
@@ -11,20 +11,25 @@ def gen_ref_n_points():
     )
     return {'ref':ref, 'points':points}
 
-def gen_bmfrm():
-    from bmfrm import Beamformer
-    bmfrm = Beamformer()
-    bmfrm.c = 2
-    return bmfrm
-
 def test_rxengine_values():
     from bmfrm.trigengines import rxengine
-    dist = rxengine(gen_bmfrm(), **gen_ref_n_points())
-    dist_known = np.array([0, np.sqrt(2)/2, np.sqrt(2)/2, 0.5])
-    assert dist == pytest.approx(dist_known)
+    c = 2
+    tau = rxengine(c, **gen_ref_n_points())
+    tau_known = np.array([0, np.sqrt(2)/c, np.sqrt(2)/c, 1/c])
+    assert tau == pytest.approx(tau_known)
 
 def test_rxengine_length():
     from bmfrm.trigengines import rxengine
-    dist = rxengine(gen_bmfrm(), **gen_ref_n_points())
-    assert np.ndim(dist) == 1 and dist.shape[0] == 4
+    c = 2
+    tau = rxengine(c, **gen_ref_n_points())
+    assert np.ndim(tau) == 1 and tau.shape[0] == 4
 
+def test_pwtxengine_values():
+    from bmfrm.trigengines import pwtxengine
+    c = 2
+    tref = 0.5
+    theta = 0
+    phi = 0
+    tau = pwtxengine(c, tref, theta, phi, **gen_ref_n_points())
+    tau_known = np.array([1, 1, 1, 2])/c
+    assert tau == pytest.approx(tau_known)
